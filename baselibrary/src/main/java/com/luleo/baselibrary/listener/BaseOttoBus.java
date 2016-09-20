95e364e78fb3d55e54a4afb55b4dde767941a8ff
han361769045 <Qiang2Han513>
2016-09-20 08:11:02 +0800

    111111111111

--- baselibrary/src/main/java/com/luleo/baselibrary/listener/BaseOttoBus.java
+++ baselibrary/src/main/java/com/luleo/baselibrary/listener/BaseOttoBus.java
@@ -4,13 +4,10 @@ import android.util.Log;
 
 import com.squareup.otto.Bus;
 
-import org.androidannotations.annotations.EBean;
-
 /**
  * Created by leo on 2015/8/23.
  */
 
-@EBean
 public abstract class BaseOttoBus extends Bus {
 
     private final String TAG = this.getClass().getPackage().getName();
